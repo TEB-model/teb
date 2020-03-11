681883c278435a0e5b1e378ad78e9ca6d383d63b
dmey <dmey@users.noreply.github.com>
2020-03-11 12:05:08 +0000

    update SFX_LIC to CeCILL version 2.1 as per email

--- src/teb/mode_surf_snow_frac.F90
+++ src/teb/mode_surf_snow_frac.F90
@@ -1,7 +1,7 @@
 !SFX_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
-!SFX_LIC This is part of the SURFEX software governed by the CeCILL-C licence
-!SFX_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
-!SFX_LIC for details. version 1.
+!SFX_LIC This is part of the SURFEX software governed by the CeCILL licence
+!SFX_LIC version 2.1. See Licence_CeCILL_V2.1-en.txt and Licence_CeCILL_V2.1-fr.txt  
+!SFX_LIC for details.
 !     ##########################
       MODULE MODE_SURF_SNOW_FRAC
 !     ##########################
