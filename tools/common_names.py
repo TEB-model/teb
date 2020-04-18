740c799e859821b8e84395b70f5fe9981b1307fa
dmey <dmey@users.noreply.github.com>
2020-04-18 16:57:30 +0200

    Update TEB common names

--- tools/common_names.py
+++ tools/common_names.py
@@ -3,11 +3,11 @@
 
 standard_quantity_names = {
     "Atmospheric pressure": {
-        "unit": "hPa",
+        "unit": "\\rm Pa",
         "symbol": "p"
     },
     "Dry-bulb air temperature": {
-        "unit": "K",
+        "unit": "\\rm K",
         "symbol": "T"
     },
     "Relative humidity": {
@@ -15,107 +15,107 @@ standard_quantity_names = {
         "symbol": "\\rm RH"
     },
     "Total sky shortwave radiation flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "S⇊"
     },
     "Sky diffuse shortwave radiation flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "S⇓"
     },
     "Sky direct shortwave radiation flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "S↓"
     },
     "Surface reflected shortwave radiation flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "S↑"
     },
     "Sky longwave radiation flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "L↓"
     },
     "Surface longwave radiation flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "L↑"
     },
     "Surface net radiative flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "Q^*"
     },
     "Wind speed": {
-        "unit": "m\ s^{-1}",
+        "unit": "\\rm m\ s^{-1}",
         "symbol": "U"
     },
     "Wind direction": {
-        "unit": "rad",
+        "unit": "\\rm rad",
         "symbol": "U_{\\theta}"
     },
     "CO2 concentration": {
-        "unit": "g\ m^{-3}",
+        "unit": "\\rm g\ m^{-3}",
         "symbol": "CO_{2}"
     },
     "Mass mixing ratio of water vapour": {
-        "unit": "g\ kg^{-1}",
+        "unit": "\\rm g\ kg^{-1}",
         "symbol": "r"
     },
     "Surface turbulent sensible heat flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "H"
     },
     "Surface turbulent latent heat flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "LE"
     },
     "Rain rate": {
-        "unit": "mm\ h^{-1}",
+        "unit": "\\rm mm\ h^{-1}",
         "symbol": "RR"
     },
     "Buildings\' energy demand for cooling": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "E_{\\rm cooling}"
     },
     "Buildings\' energy demand for heating": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "E_{\\rm heating}"
     },
     "Thermal energy production of solar panels on roofs": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "Q_{\\rm solar}"
     },
     "Electrical energy production of solar panels on roofs": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "E_{\\rm solar}"
     },
     "Dry-bulb air temperature at half building height": {
-        "unit": "K",
+        "unit": "\\rm K",
         "symbol": "T_{\\rm canyon}"
     },
     "Mass mixing ratio of water vapour at half building height": {
-        "unit": "kg\ kg^{-1}",
+        "unit": "\\rm kg\ kg^{-1}",
         "symbol": "r_{\\rm canyon}"
     },
     "Canyon horizontal wind speed": {
-        "unit": "m\ s^{-1}",
+        "unit": "\\rm m\ s^{-1}",
         "symbol": "U_{\\rm canyon}"
     },
     "Canyon horizontal wind direction": {
-        "unit": "rad",
+        "unit": "\\rm rad",
         "symbol": "U_{\\theta_{canyon}}"
     },
     "Zonal component of wind velocity at half building height": {
-        "unit": "m\ s^{-1}",
+        "unit": "\\rm m\ s^{-1}",
         "symbol": "u_{\\rm canyon}"
     },
     "Meridional component of wind velocity at half building height": {
-        "unit": "m\ s^{-1}",
+        "unit": "\\rm m\ s^{-1}",
         "symbol": "v_{\\rm canyon}"
     },
     "Zonal component of wind velocity": {
-        "unit": "m\ s^{-1}",
+        "unit": "\\rm m\ s^{-1}",
         "symbol": "u"
     },
     "Meridional component of wind velocity": {
-        "unit": "m\ s^{-1}",
+        "unit": "\\rm m\ s^{-1}",
         "symbol": "v"
     },
     "Surface emissivity": {
@@ -127,27 +127,27 @@ standard_quantity_names = {
         "symbol": "\\alpha"
     },
     "Surface evaporative moisture flux density": {
-        "unit": "kg\ m^{-2}\ s^{-1}",
+        "unit": "\\rm kg\ m^{-2}\ s^{-1}",
         "symbol": "E"
     },
     "Surface ground heat flux density": {
-        "unit": "W\ m^{-2}",
+        "unit": "\\rm W\ m^{-2}",
         "symbol": "G"
     },
-    "Surface skin temperature": {
+    "Surface (skin) temperature": {
         "unit": "1",
         "symbol": "T_{s}"
     },
     "Specific humidity": {
-        "unit": "kg\ kg^{-1}",
+        "unit": "\\rm kg\ kg^{-1}",
         "symbol": "q"
     },
     "Canyon specific humidity": {
-        "unit": "kg\ kg^{-1}",
+        "unit": "\\rm kg\ kg^{-1}",
         "symbol": "q_{\\rm canyon}"
     },
     "Surface friction velocity": {
-        "unit": "m\ s^{-1}",
+        "unit": "\\rm m\ s^{-1}",
         "symbol": "u_{\\star}"
     }
 }
@@ -183,7 +183,16 @@ teb_quantity_names_output = {
     'Surface friction velocity': 'USTAR_TOWN',
     'Surface albedo': 'ALB_TOWN',
     'Surface emissivity': 'EMIS_TOWN',
-    'Surface skin temperature': 'TS_TOWN',
+    'Surface (skin) temperature': 'TS_TOWN',
     'Thermal energy production of solar panels on roofs': 'THER_PROD_PANEL',
     'Electrical energy production of solar panels on roofs': 'PHOT_PROD_PANEL',
+    'Canyon atmospheric pressure': 'P_CANYON',
+    'Buildings\' internal dry-bulb air temperature': 'TI_BLD',
+    'Road surface (skin) temperature': 'T_ROAD1',
+    'Roof surface (skin) temperature': 'T_ROOF1',
+    'Wall A surface (skin) temperature': 'T_WALLA1',
+    'Wall B surface (skin) temperature': 'T_WALLB1',
 }
+
+
+teb_quantity_names_output_by_keys = {v:k for k,v in teb_quantity_names_output.items()}
