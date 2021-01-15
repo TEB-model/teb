4786c7146a8a57821b3cad7f6e16e30e04a5a60c
dmey <dmey@users.noreply.github.com>
2021-01-15 09:55:45 +0100

    CeCILL_V2.1 for Open SURFEX files

--- src/teb/dx_air_cooling_coil_cv.F90
+++ src/teb/dx_air_cooling_coil_cv.F90
@@ -1,6 +1,6 @@
 !SFX_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
-!SFX_LIC This is part of the SURFEX software governed by the CeCILL-C licence
-!SFX_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
+!SFX_LIC This is part of the SURFEX software governed by the CeCILL version 2.1
+!SFX_LIC version 1. See LICENSE, Licence_CeCILL_V2.1-en.txt and Licence_CeCILL_V2.1-fr.txt  
 !SFX_LIC for details. version 1.
 SUBROUTINE DX_AIR_COOLING_COIL_CV(PT_CANYON, PQ_CANYON, PPS, PRHOA,    &
                              PT_IN, PQ_IN, PCOP_RAT, PCAP_SYS_RAT,     &
--- src/teb/modd_reprod_oper.F90
+++ src/teb/modd_reprod_oper.F90
@@ -1,6 +1,6 @@
 !SFX_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
-!SFX_LIC This is part of the SURFEX software governed by the CeCILL-C licence
-!SFX_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
+!SFX_LIC This is part of the SURFEX software governed by the CeCILL version 2.1
+!SFX_LIC version 1. See LICENSE, Licence_CeCILL_V2.1-en.txt and Licence_CeCILL_V2.1-fr.txt  
 !SFX_LIC for details. version 1.
 !     ######################
       MODULE MODD_REPROD_OPER
